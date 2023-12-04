b4a5fbe9be864ce39dac941a664ac67e32887c47
ewanjlynch <123566101+ewanjlynch@users.noreply.github.com>
2023-12-04 16:05:04 +0000

    updates

--- scripts.js
+++ scripts.js
@@ -1,3 +1,10 @@
+let playerScore = 0
+let computerScore = 0
+let winner = ''
+
+
+// Computer selection randomiser function
+
 function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
@@ -12,3 +19,46 @@ function getComputerChoice() {
 
 const computerSelection = getComputerChoice();
 console.log(computerSelection);
+
+
+// group winning combos together => trigger win for either computer or player. 2 sets of 3
+
+function playRound(playerSelection, computerSelection)
+    if (playerSelection === computerSelection) {
+        winner = 'tie'
+    }
+    if ( 
+        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+        (playerSelection === 'Scissors' && computerSelection === 'Paper') 
+    ) {
+        playerScore++;
+        winner = 'player';
+    }
+
+
+    if ( 
+        (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+        (computerSelection === 'Paper' && playerSelection === 'Rock') ||
+        (computerSelection === 'Scissors' && playerSelection === 'Paper') 
+    ) {
+        computerScore++;
+        winner = 'computer';
+    }
+
+    const rockbutton = document.getElementById('rockbutton')
+    const paperbutton = document.getElementById('paperbutton')
+    const scissorsbutton = document.getElementById('scissorsbutton')
+
+    rockbutton.addEventListener('click', () => {
+        const computerSelection = getComputerChoice()
+        playRound(playerSelection, computerSelection)
+    })
+    paperbutton.addEventListener('click', () => {
+        const computerSelection = getComputerChoice()
+        playRound(playerSelection, computerSelection)
+    })
+    scissorsbutton.addEventListener('click', () => {
+        const computerSelection = getComputerChoice()
+        playRound(playerSelection, computerSelection)
+    })
