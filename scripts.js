421426f89ce60f178121e5b5cf4801ed1669c44c
ewanjlynch <123566101+ewanjlynch@users.noreply.github.com>
2023-12-05 12:53:55 +0000

    updates

--- scripts.js
+++ scripts.js
@@ -1,64 +1,47 @@
-let playerScore = 0
-let computerScore = 0
-let winner = ''
-
-
-// Computer selection randomiser function
-
-function getComputerChoice() {
+    function getComputerChoice() {
     let choice = Math.floor(Math.random() * 3);
 
     if (choice === 0) {
         return "Rock";
     } else if (choice === 1) {
         return "Paper";
-    } else {
+    } else {                 
         return "Scissors";
-    }
+    }             
+  }
+
+let playerScore = 0
+let computerScore = 0
+let winner = ''
+
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+            winner = 'tie'
+            return winner
+        }
+        if ( 
+            (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
+            (playerSelection === 'Paper' && computerSelection === 'Rock') ||
+            (playerSelection === 'Scissors' && computerSelection === 'Paper') 
+        ) {
+            playerScore++;
+            winner = 'player wins!';
+            return winner
+        }
+    
+    
+        if ( 
+            (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
+            (computerSelection === 'Paper' && playerSelection === 'Rock') ||
+            (computerSelection === 'Scissors' && playerSelection === 'Paper') 
+         ) {
+            computerScore++;
+            winner = 'computer wins!';
+            return winner
+         }
+        
 }
 
+const playerSelection = "Rock";
 const computerSelection = getComputerChoice();
-console.log(computerSelection);
-
-
-// group winning combos together => trigger win for either computer or player. 2 sets of 3
-
-function playRound(playerSelection, computerSelection)
-    if (playerSelection === computerSelection) {
-        winner = 'tie'
-    }
-    if ( 
-        (playerSelection === 'Rock' && computerSelection === 'Scissors') ||
-        (playerSelection === 'Paper' && computerSelection === 'Rock') ||
-        (playerSelection === 'Scissors' && computerSelection === 'Paper') 
-    ) {
-        playerScore++;
-        winner = 'player';
-    }
-
-
-    if ( 
-        (computerSelection === 'Rock' && playerSelection === 'Scissors') ||
-        (computerSelection === 'Paper' && playerSelection === 'Rock') ||
-        (computerSelection === 'Scissors' && playerSelection === 'Paper') 
-    ) {
-        computerScore++;
-        winner = 'computer';
-    }
-
-    const rockbutton = document.getElementById('rockbutton')
-    const paperbutton = document.getElementById('paperbutton')
-    const scissorsbutton = document.getElementById('scissorsbutton')
-
-    rockbutton.addEventListener('click', () => {
-        const computerSelection = getComputerChoice()
-        playRound(playerSelection, computerSelection)
-    })
-    paperbutton.addEventListener('click', () => {
-        const computerSelection = getComputerChoice()
-        playRound(playerSelection, computerSelection)
-    })
-    scissorsbutton.addEventListener('click', () => {
-        const computerSelection = getComputerChoice()
-        playRound(playerSelection, computerSelection)
-    })
+console.log(playRound(playerSelection, computerSelection));
